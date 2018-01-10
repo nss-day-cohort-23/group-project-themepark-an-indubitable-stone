c899840d19b615c4e6a357b7aafb142dcab7c7c0
Tim A. Conner <timaconner1@gmail.com>
2018-01-10 14:06:57 -0600

    add setFooterDate.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 "use strict";
+const $ = require("jquery");
 
+
+const setFooterDate = () => {
+    let todaysDate = new Date(Date.now()),
+    day = todaysDate.getDay(),
+    month = todaysDate.getMonth(),
+    year = todaysDate.getFullYear();
+
+    $("#footer-date").text(`${day}${month}${year}`);
+};
+
+
+setFooterDate();
