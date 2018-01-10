53049929a621bb27f54b27c62912a01ce166400c
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-10 14:18:47 -0600

    Initializes draft ajax for getParkInfo

--- js/model.js
+++ js/model.js
@@ -1,12 +1,21 @@
 'use strict';
 const $ = require('jquery');
 
+const fbURL = "https://an-indubitable-stone.firebaseio.com/"
 module.exports.getParkInfo = function() {
-
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: `${fbURL}/park-info`
+    })
+    .done(data => resolve(data))
+    .fail(err => reject(err));
+  });
 };
 
 module.exports.getAreas = function() {
+  return new Promise(function(resolve, reject) {
 
+  });
 };
 
 module.exports.getAttractions = function() {
